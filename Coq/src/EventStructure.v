78d4e34130c2cd6d25b6448edada25348cb84360
Podkopaev Anton <podkoav239@gmail.com>
2018-09-06 15:29:20 +0300

    Coq. Added some version of continuations to ES.

--- Coq/src/EventStructure.v
+++ Coq/src/EventStructure.v
@@ -3,12 +3,31 @@ From promising Require Import Basic.
 From imm Require Import Events.
 Require Import AuxRel.
 
-Definition eventid := Ident.t.
+Definition eventid := nat.
 
-(** Definition of an execution *)
+Module Language.
+Record t :=
+  mk { syntax : Type;
+       state : Type;
+       init : syntax -> state;
+       is_terminal : state -> Prop;
+       step : list label -> state -> state -> Prop
+     }.
+End Language.
+
+Module Continuation.
+Record t :=
+  { lang     : Language.t;
+    state    : lang.(Language.state);
+    thread   : thread_id;
+    prev_act : option eventid;
+  }.
+End Continuation.
+
 Record event_structure :=
   { acts : list eventid;
     acts_init : list eventid;
+    next_act : eventid;
     tid  : eventid -> thread_id;
     lab  : eventid -> label;
     sb   : eventid -> eventid -> Prop ;
@@ -16,6 +35,7 @@ Record event_structure :=
     rf : eventid -> eventid -> Prop ;
     co : eventid -> eventid -> Prop ;
     ew : eventid -> eventid -> Prop ;
+    cons : list Continuation.t;
   }.
 
 Section EventStructure.
@@ -24,9 +44,12 @@ Variable ES : event_structure.
 
 Definition acts_set := fun x => In x ES.(acts).
 Definition acts_init_set := fun x => In x ES.(acts_init).
+Definition cons_set := fun x => In x ES.(cons).
 
 Notation "'E'" := acts_set.
-Notation "'E_init'" := acts_init_set.
+Notation "'Einit'" := acts_init_set.
+Notation "'K'" := cons_set.
+Notation "'next_act'" := ES.(next_act).
 Notation "'lab'" := ES.(lab).
 Notation "'sb'" := ES.(sb).
 Notation "'rmw'" := ES.(rmw).
@@ -54,7 +77,7 @@ Notation "'Acq'" := (is_acq lab).
 Notation "'Acqrel'" := (is_acqrel lab).
 Notation "'Sc'" := (is_sc lab).
 
-Definition cf := (sb⁻¹ ;; <| E_init |> ;; sb) \ sb ⁼.
+Definition cf := (sb⁻¹ ;; <| Einit |> ;; sb) \ sb ⁼.
 
 Hint Unfold cf : unfolderDb. 
 
@@ -62,8 +85,8 @@ Lemma cf_irr : irreflexive cf.
 Proof. basic_solver. Qed.
 
 Record Wf :=
-  { initD : E_init ⊆₁ E;
-    init_lab : forall e (INIT : E_init e), exists l, lab e = Astore Xpln Opln l 0 ;
+  { initD : Einit ⊆₁ E;
+    init_lab : forall e (INIT : Einit e), exists l, lab e = Astore Xpln Opln l 0 ;
     sbE : sb ≡ ⦗E⦘ ⨾ sb ⨾ ⦗E⦘ ;
     rmwD : rmw ≡ ⦗R⦘ ⨾ rmw ⨾ ⦗W⦘ ;
     rmwl : rmw ⊆ same_loc ;
@@ -86,6 +109,21 @@ Record Wf :=
     ewc : ew ⊆ cf ;
     ew_trans : transitive ew ;
     ew_sym : symmetric ew ;
+    next_act_lt : forall e (EE : E e), lt e next_act;
+
+    cons_prev_ninit : forall c (CC : K c) e (PA : c.(Continuation.prev_act) = Some e),
+        ~ Einit e;
+    cons_prevE : forall c (CC : K c) e (PA : c.(Continuation.prev_act) = Some e), E e;
+
+    cons_uniq_prev : forall c c' (CC : K c) (CC' : K c') e
+                            (PA : c.(Continuation.prev_act) = Some e),
+        c = c' \/
+        c.(Continuation.prev_act) <> c'.(Continuation.prev_act);
+    cons_uniq_tid : forall c c' (CC : K c) (CC' : K c')
+                            (PA  : c .(Continuation.prev_act) = None)
+                            (PA' : c'.(Continuation.prev_act) = None),
+        c = c' \/
+        c.(Continuation.thread) <> c'.(Continuation.thread);
   }.
 
 Implicit Type WF : Wf.
