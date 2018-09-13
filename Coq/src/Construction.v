8e6095b9652d990f912753d981f5f373849279c3
Podkopaev Anton <podkoav239@gmail.com>
2018-09-13 12:27:37 +0200

    Coq. minor

--- Coq/src/Construction.v
+++ Coq/src/Construction.v
@@ -42,10 +42,10 @@ Definition t_basic
   << PRCL : s'.(ES.sb) ≡ <| s'.(ES.acts_set) |> ;; s'.(ES.sb) >> /\
   << REP  : s' = ES.mk (event_list ++ s.(ES.acts))
                        (rmw_edge ∪ s.(ES.rmw))
-                       s.(ES.rf) s.(ES.co) s.(ES.ew)
+                       s.(ES.jf) s.(ES.co) s.(ES.ew)
   >>.
   
-Definition add_rf (r : EventId.t) (s s' : ES.t) : Prop :=
+Definition add_jf (r : EventId.t) (s s' : ES.t) : Prop :=
   << RR : R r >> /\
   exists w,
     << EW   : s.(ES.acts_set) w >> /\
@@ -54,7 +54,7 @@ Definition add_rf (r : EventId.t) (s s' : ES.t) : Prop :=
     << VAL  : same_val w r >> /\
     << REPR :
       s' = ES.mk s.(ES.acts) s.(ES.rmw)
-                 (singl_rel w r ∪ s.(ES.rf))
+                 (singl_rel w r ∪ s.(ES.jf))
                  s.(ES.co) s.(ES.ew)
     >>.
 
@@ -67,7 +67,7 @@ Definition add_ew (w : EventId.t) (s s' : ES.t) : Prop :=
     << CFWS  : ws ⊆₁ s.(ES.cf) w >> /\
     << REPR :
       s' = ES.mk s.(ES.acts) s.(ES.rmw)
-                 s.(ES.rf) s.(ES.co)
+                 s.(ES.jf) s.(ES.co)
                  (ws × eq w ∪ eq w × ws ∪ s.(ES.ew))
     >>.
 
@@ -78,7 +78,7 @@ Definition add_co (w : EventId.t) (s s' : ES.t) : Prop :=
     << WWS : ws ⊆₁ A >> /\
     << REPR :
       s' = ES.mk s.(ES.acts) s.(ES.rmw)
-                 s.(ES.rf)
+                 s.(ES.jf)
                  (ws × eq w ∪ eq w × (A \₁ ws) ∪ s.(ES.co))
                  s.(ES.ew)
     >>.
@@ -86,12 +86,12 @@ Definition add_co (w : EventId.t) (s s' : ES.t) : Prop :=
 Inductive t_ (execs : thread_id -> state -> Prop) (s s' : ES.t) : Prop :=
 | t_fence  e  (FF : F e) (BS : t_basic execs e None s s')
 | t_load   e s1          (BS : t_basic execs e None s s1)
-           (RF : add_rf e s1 s')
+           (JF : add_jf e s1 s')
 | t_store  e s1 s2       (BS : t_basic execs e None s s1)
            (EW : add_ew e s1 s2)
            (CO : add_co e s2 s')
 | t_update e e' s1 s2 s3 (BS : t_basic execs e (Some e') s s1)
-           (RF : add_rf e  s1 s2)
+           (JF : add_jf e  s1 s2)
            (EW : add_ew e' s2 s3)
            (CO : add_co e' s3 s').
 
