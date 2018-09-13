602c0420ece390945dc8d606341ad2120a3e224c
Podkopaev Anton <podkoav239@gmail.com>
2018-09-13 14:16:51 +0200

    Coq. simrel_common (I_base) is defined.

--- Coq/src/SimRel.v
+++ Coq/src/SimRel.v
@@ -10,17 +10,25 @@ Section SimRel.
   
   Notation "'C'" := (covered TC).
   Notation "'I'" := (issued TC).
+  Notation "'Gsb'" := (G.(sb)).
+  Notation "'Grf'" := (G.(rf)).
+  Notation "'Gco'" := (G.(co)).
+  Notation "'Ssb'" := (S.(ES.sb)).
+  Notation "'Srf'" := (S.(ES.rf)).
+  Notation "'Sco'" := (S.(ES.co)).
+  Notation "'Scf'" := (S.(ES.cf)).
 
+  (* TODO. Probably, we need the following condition:
+             s is injective on C ∪₁ I
+   *)
   Record simrel_common :=
     { labEq : forall e (CI : (C ∪₁ I) e),
         EventId.lab e.(f) = G.(lab) e;
-      sbPrcl : S.(ES.sb) ;; <| f ∘₁ C |> ⊆ <| f ∘₁ C |> ;; S.(ES.sb);
-      sbF : f ∘ (<| C |> ;; G.(sb) ;; <| C |>) ≡
-              <| f ∘₁ C |> ;; S.(ES.sb) ;; <| f ∘₁ C |>;
-      cimgNcf : <| f ∘₁ C |> ;; S.(ES.cf) ;; <| f ∘₁ C |> ≡ ∅₂;
-
-
-      (* TODO. Probably, we need the following condition:
-             s is injective on C ∪₁ I *)
+      sbPrcl : Ssb ;; <| f ∘₁ C |> ⊆ <| f ∘₁ C |> ;; Ssb;
+      sbF : f ∘ (<| C |> ;; Gsb ;; <| C |>) ≡
+            <| f ∘₁ C |> ;; Ssb ;; <| f ∘₁ C |>;
+      cimgNcf : <| f ∘₁ C |> ;; Scf ;; <| f ∘₁ C |> ≡ ∅₂;
+      imgrf : f ∘ (<| I |> ;; Grf ;; <| C |>) ≡ Srf;
+      imgco : f ∘ (<| I |> ;; Gco ;; <| I |>) ⊆ Sco;
     }.
 End SimRel.
