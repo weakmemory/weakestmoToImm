edff713a8dbe983ee2d310952b967cb1c51b1b0a
Podkopaev Anton <podkoav239@gmail.com>
2018-10-01 15:01:06 +0200

    Coq. Working on certification graph.

--- Coq/src/SimRelCert.v
+++ Coq/src/SimRelCert.v
@@ -19,7 +19,8 @@ Section SimRelCert.
   
   Variable f : actid -> eventid.
   Variable h : actid -> eventid.
-  Variable q : actid.
+  Variable q : ES.cont_label.
+  Notation "'qtid'" := (ES.cont_thread S q) (only parsing).
 
   (* A state, which is reachable from a state in a continuation related to (h q) in S
      and which represents a graph certification. *)
@@ -54,6 +55,7 @@ Section SimRelCert.
   Notation "'Grmw'" := G.(rmw).
 
   Notation "'Gtid_' t" := (fun x => tid x = t) (at level 1).
+  Notation "'GNtid_' t" := (fun x => tid x <> t) (at level 1).
 
   Notation "'GR'" := (fun a => is_true (is_r Glab a)).
   Notation "'GW'" := (fun a => is_true (is_w Glab a)).
@@ -72,17 +74,17 @@ Section SimRelCert.
   Notation "'C''"  := (covered TC').
   Notation "'I''"  := (issued TC').
 
-  Record sim_cert_graph thread :=
-    { cslab : eq_dom ((Gtid_ thread) ∩₁ (C' ∪₁ I')) certLab Glab;
-      cuplab : forall e (TIDE : Gtid_ thread e)
+  Record sim_cert_graph :=
+    { cslab : eq_dom ((Gtid_ qtid) ∩₁ (C' ∪₁ I')) certLab Glab;
+      cuplab : forall e (TIDE : Gtid_ qtid e)
                       (DOMI : dom_rel (Gsb ;; <| I' |>) e),
           same_label_up_to_value (certLab e) (Glab e);
       cstate_reachable :
-        forall (state : (thread_lts (Gtid q)).(Language.state))
-               (KK : K (ES.CEvent (h q), existT _ _ state)),
-          (step (Gtid q))^* state state';
+        forall (state : (thread_lts qtid).(Language.state))
+               (KK : K (q, existT _ _ state)),
+          (step qtid)^* state state';
       
-      dcertE : certE ≡₁ Gtid_ (Gtid q) ∩₁ dom_rel (Gsb^? ;; <| C' ∪₁ I' |>);
+      dcertE : certE ≡₁ Gtid_ qtid ∩₁ dom_rel (Gsb^? ;; <| C' ∪₁ I' |>);
       dcertRMW : certRmw ≡ <| certE |> ;; Grmw ;; <| certE |>;
 
       (* TODO. Reflect the following properties of the certification graph: *)
@@ -102,26 +104,32 @@ Section SimRelCert.
     { inGi  : Gtid_ i e;
       sbMAX : forall e', Gtid_ i e' -> Gsb^? e' e
     }.
+  
+  Definition sbq_dom :=
+    match q with
+    | ES.CInit  _ => ∅
+    | ES.CEvent e => Gtid_ qtid ∩₁ dom_rel (Gsb^? ;; <| eq (g e) |>)
+    end.
     
-  Notation "'hdom'" := (Gtid_ (tid q) ∩₁ dom_rel (Gsb^? ⨾ ⦗ I' ⦘)) (only parsing).
+  Notation "'hdom'" := (C ∪₁ (I ∩₁ GNtid_ qtid) ∪₁ sbq_dom) (only parsing).
       
   Record simrel_cert :=
     { sim : simrel prog S G sc TC f;
 
-      cert : sim_cert_graph (tid q); 
+      cert : sim_cert_graph;
 
       tr_step :
         exists e,
-          << TIDE : Gtid e = Gtid q>> /\
+          << TIDE : Gtid e = qtid >> /\
           << TCSTEP : itrav_step G sc e TC TC' >>;
 
       hgtrip : ⦗ hdom ⦘ ⨾ ↑ (compose g h) ⊆ eq;
 
-      hinj : inj_dom hdom h;  
+      hinj : inj_dom hdom h;
       himg : h □₁ hdom ⊆₁ SE;
       hoth : (h □₁ set_compl hdom) ∩₁ SE ≡₁ ∅;
       hlab : eq_dom hdom certLab (compose Slab h);
-      htid : eq_dom hdom Gtid (compose Stid h); 
+      htid : compose Stid h = Gtid;
 
       hco : h □ ⦗ hdom ⦘ ⨾ Gco ⨾ ⦗ hdom ⦘ ⊆ Sco;
 
@@ -130,20 +138,81 @@ Section SimRelCert.
       complete_fdom :
         (h □₁ hdom) ∩₁ SR ⊆₁ codom_rel (⦗ h □₁ hdom ⦘ ⨾ Srf);
 
-      imgcc : ⦗ f □₁ dom_rel (Gsb^? ⨾ ⦗ eq q ⦘) ⦘ ⨾ Scc ⨾ ⦗ Stid_ (tid q) ⦘ ⊆ 
+      imgcc : ⦗ f □₁ sbq_dom ⦘ ⨾ Scc ⨾ ⦗ Stid_ qtid ⦘ ⊆
               ⦗ h □₁ GW ⦘ ⨾ Sew ⨾ Ssb^= ;
     }.
-
 End SimRelCert.
 
-Lemma simrel_cert_start prog S G sc TC TC' f (*certG*) i 
-      (SR : simrel prog S G sc TC f)
-      (TR_STEP : tid_trav_step G sc TC TC' i) : 
-  exists q q', 
-    pc S TC f i q' /\
-    simrel_cert prog S G sc TC TC' f f q.
+Section SimRelLemmas.
+
+Variable prog : Prog.t.
+Variable S : ES.t.
+Variable G  : execution.
+Variable GPROG : program_execution prog G.
+Variable sc : relation actid.
+Variable TC : trav_config.
+
+Variable f : actid -> eventid.
+
+Notation "'g'" := (event_to_act S).
+
+Notation "'SE'" := S.(ES.acts_set).
+Notation "'SEinit'" := S.(ES.acts_init_set).
+Notation "'Stid'" := (S.(ES.tid)).
+Notation "'Slab'" := (S.(ES.lab)).
+Notation "'Sloc'" := (loc S.(ES.lab)).
+Notation "'K'"  := S.(ES.cont_set).
+
+Notation "'Stid_' t" := (fun x => Stid x = t) (at level 1).
+
+Notation "'Ssb'" := (S.(ES.sb)).
+Notation "'Srf'" := (S.(ES.rf)).
+Notation "'Sco'" := (S.(ES.co)).
+Notation "'Scf'" := (S.(ES.cf)).
+Notation "'Scc'" := (S.(ES.cc)).
+Notation "'Sew'" := (S.(ES.ew)).
+
+Notation "'SR'" := (fun a => is_true (is_r Slab a)).
+Notation "'SW'" := (fun a => is_true (is_w Slab a)).
+
+Notation "'GE'" := G.(acts_set).
+Notation "'Glab'" := (G.(lab)).
+Notation "'Gtid'" := (tid).
+Notation "'Grmw'" := G.(rmw).
+
+Notation "'Gtid_' t" := (fun x => tid x = t) (at level 1).
+Notation "'GNtid_' t" := (fun x => tid x <> t) (at level 1).
+
+Notation "'GR'" := (fun a => is_true (is_r Glab a)).
+Notation "'GW'" := (fun a => is_true (is_w Glab a)).
+
+Notation "'Gsb'" := (G.(sb)).
+Notation "'Ghb'" := (G.(imm_hb.hb)).
+Notation "'Grf'" := (G.(rf)).
+Notation "'Gco'" := (G.(co)).
+
+Notation "'C'"  := (covered TC).
+Notation "'I'"  := (issued TC).
+
+Variable SRC : simrel prog S G sc TC f.
+
+Lemma sim_cert_graph_start TC' e
+      (TR_STEP : itrav_step G sc e TC TC') : 
+  exists q state',
+    << QTID : ES.cont_thread S q = tid e >> /\
+    << SRCG : sim_cert_graph S G TC' q state' >>.
 Proof.
-  admit.
+Admitted.
+
+Lemma simrel_cert_start TC' e
+      (TR_STEP : itrav_step G sc e TC TC') : 
+  exists q state',
+    << SRCC : simrel_cert prog S G sc TC TC' f f q state' >>.
+Proof.
+  edestruct sim_cert_graph_start as [q [state' HH]]; eauto.
+  desf.
+  exists q. exists state'.
+  constructor; auto.
 Admitted.
 
 Lemma simrel_cert_end prog S G sc TC TC' f h (*certG*) i q
@@ -152,7 +221,6 @@ Lemma simrel_cert_end prog S G sc TC TC' f h (*certG*) i q
   exists f', 
     simrel prog S G sc TC' f'.
 Proof.
-  admit.
 Admitted.
 
 Lemma simrel_cert_step prog S G sc TC TC' f h (*certG*) i q
@@ -162,7 +230,6 @@ Lemma simrel_cert_step prog S G sc TC TC' f h (*certG*) i q
     (ESstep.t Weakestmo)^? S S' /\
     simrel_cert prog S' G sc TC TC' f h' q'.
 Proof.
-  admit.
 Admitted.
-    
-    
+
+End SimRelLemmas.
