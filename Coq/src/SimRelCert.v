e0b2c89fdc9635994a50dd7a76bba858694f441d
Podkopaev Anton <podkoav239@gmail.com>
2018-10-01 12:21:36 +0200

    Coq. Working on a certification graph part of SimRelCert.

--- Coq/src/SimRelCert.v
+++ Coq/src/SimRelCert.v
@@ -12,7 +12,6 @@ Section SimRelCert.
   Variable prog : Prog.t.
   Variable S : ES.t.
   Variable G  : execution.
-  Variable certG : execution. 
   Variable GPROG : program_execution prog G.
   Variable sc : relation actid.
   Variable TC : trav_config.
@@ -22,9 +21,13 @@ Section SimRelCert.
   Variable h : actid -> eventid.
   Variable q : actid.
 
-  Notation "'g'" := (event_to_act S).
+  (* A state, which is reachable from a state in a continuation related to (h q) in S
+     and which represents a graph certification. *)
+  Variable state' : state.
+  
+  Notation "'certG'" := state'.(ProgToExecution.G).
 
-  Notation "'qtid'" := (tid q).
+  Notation "'g'" := (event_to_act S).
 
   Notation "'SE'" := S.(ES.acts_set).
   Notation "'SEinit'" := S.(ES.acts_init_set).
@@ -48,6 +51,7 @@ Section SimRelCert.
   Notation "'GE'" := G.(acts_set).
   Notation "'Glab'" := (G.(lab)).
   Notation "'Gtid'" := (tid).
+  Notation "'Grmw'" := G.(rmw).
 
   Notation "'Gtid_' t" := (fun x => tid x = t) (at level 1).
 
@@ -59,40 +63,64 @@ Section SimRelCert.
   Notation "'Grf'" := (G.(rf)).
   Notation "'Gco'" := (G.(co)).
 
-  Notation "'certGE'" := G.(acts_set).
-  Notation "'certGlab'" := (G.(lab)).
+  Notation "'certE'" := certG.(acts_set).
+  Notation "'certLab'" := (certG.(lab)).
+  Notation "'certRmw'" := (certG.(rmw)).
   
   Notation "'C'"  := (covered TC).
   Notation "'I'"  := (issued TC).
   Notation "'C''"  := (covered TC').
   Notation "'I''"  := (issued TC').
 
-  Record cert_graph thread :=
-    { same_lab : eq_dom ((Gtid_ thread) ∩₁ (C' ∪₁ I')) Glab certGlab; }.
+  Record sim_cert_graph thread :=
+    { cslab : eq_dom ((Gtid_ thread) ∩₁ (C' ∪₁ I')) certLab Glab;
+      cuplab : forall e (TIDE : Gtid_ thread e)
+                      (DOMI : dom_rel (Gsb ;; <| I' |>) e),
+          same_label_up_to_value (certLab e) (Glab e);
+      cstate_reachable :
+        forall (state : (thread_lts (Gtid q)).(Language.state))
+               (KK : K (ES.CEvent (h q), existT _ _ state)),
+          (step (Gtid q))^* state state';
+      
+      dcertE : certE ≡₁ Gtid_ (Gtid q) ∩₁ dom_rel (Gsb^? ;; <| C' ∪₁ I' |>);
+      dcertRMW : certRmw ≡ <| certE |> ;; Grmw ;; <| certE |>;
+
+      (* TODO. Reflect the following properties of the certification graph: *)
+      (* ⟪ NEW_VAL1 : forall r w (RF: new_rfi w r), *)
+      (*     val (s'.(G).(lab)) r = val (s'.(G).(lab)) w ⟫ /\ *)
+      (* ⟪ NEW_VAL2 : forall r (RR : is_r s'.(G).(lab) r) (IN: MOD r) *)
+      (*                     (NIN: ~ (codom_rel new_rfi) r), *)
+      (*     val (s'.(G).(lab)) r = Some (new_val r) ⟫ /\ *)
+      (* ⟪ OLD_VAL : forall a (NIN: ~ MOD a), *)
+      (*     val (s'.(G).(lab)) a = val (s.(G).(lab)) a ⟫. *)
+    }.
 
   Record sb_max i e :=
+    (* TODO. It looks too strong. Shouldn't e' be in GE at very least?
+       Doesn't inGi follow from sbMAX?
+     *)
     { inGi  : Gtid_ i e;
       sbMAX : forall e', Gtid_ i e' -> Gsb^? e' e
     }.
     
-  Definition tid_trav_step thread :=
-    exists e, Gtid e = thread /\ itrav_step G sc e TC TC'.
-
-  Notation "'hdom'" := (Gtid_ qtid ∩₁ dom_rel (Gsb^? ⨾ ⦗ I' ⦘)) (only parsing).
+  Notation "'hdom'" := (Gtid_ (tid q) ∩₁ dom_rel (Gsb^? ⨾ ⦗ I' ⦘)) (only parsing).
       
   Record simrel_cert :=
     { sim : simrel prog S G sc TC f;
 
-      cert : cert_graph qtid; 
+      cert : sim_cert_graph (tid q); 
 
-      tr_step : tid_trav_step qtid;
+      tr_step :
+        exists e,
+          << TIDE : Gtid e = Gtid q>> /\
+          << TCSTEP : itrav_step G sc e TC TC' >>;
 
       hgtrip : ⦗ hdom ⦘ ⨾ ↑ (compose g h) ⊆ eq;
 
       hinj : inj_dom hdom h;  
       himg : h □₁ hdom ⊆₁ SE;
       hoth : (h □₁ set_compl hdom) ∩₁ SE ≡₁ ∅;
-      hlab : eq_dom hdom certGlab (compose Slab h);
+      hlab : eq_dom hdom certLab (compose Slab h);
       htid : eq_dom hdom Gtid (compose Stid h); 
 
       hco : h □ ⦗ hdom ⦘ ⨾ Gco ⨾ ⦗ hdom ⦘ ⊆ Sco;
@@ -102,12 +130,11 @@ Section SimRelCert.
       complete_fdom :
         (h □₁ hdom) ∩₁ SR ⊆₁ codom_rel (⦗ h □₁ hdom ⦘ ⨾ Srf);
 
-      imgcc : ⦗ f □₁ dom_rel (Gsb^? ⨾ ⦗ eq q ⦘) ⦘ ⨾ Scc ⨾ ⦗ Stid_ qtid ⦘ ⊆ 
+      imgcc : ⦗ f □₁ dom_rel (Gsb^? ⨾ ⦗ eq q ⦘) ⦘ ⨾ Scc ⨾ ⦗ Stid_ (tid q) ⦘ ⊆ 
               ⦗ h □₁ GW ⦘ ⨾ Sew ⨾ Ssb^= ;
     }.
 
 End SimRelCert.
-
 
 Lemma simrel_cert_start prog S G sc TC TC' f (*certG*) i 
       (SR : simrel prog S G sc TC f)
