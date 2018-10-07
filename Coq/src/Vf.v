6f945015edae1ab4a72a089855a4cc262c20c030
Podkopaev Anton <podkoav239@gmail.com>
2018-10-07 15:26:42 +0300

    Coq. minor

--- Coq/src/Vf.v
+++ Coq/src/Vf.v
@@ -1,5 +1,5 @@
 From hahn Require Import Hahn.
-From imm Require Import Events Execution imm_hb imm.
+From imm Require Import Events Execution Execution_eco imm_hb imm AuxRel.
 Require Import EventStructure Consistency.
 
 Set Implicit Arguments.
@@ -41,68 +41,86 @@ Definition Gvf (G : execution) :=
 Definition Svf (m : model) (S : ES.t) :=
   Avf S.(ES.acts_set) S.(ES.lab) S.(ES.jf) S.(Consistency.hb) (Consistency.psc S m).
 
-(* Section GvfProperties. *)
-(* Variable G : execution. *)
-(* Variable WF : Wf G. *)
+Section GvfProperties.
+Variable G : execution.
+Variable WF : Wf G.
+Variable COH : coherence G.
+Variable COMP : complete G.
+Variable ACYC_EXT : acyc_ext G.
 
-(* Notation "'E'" := G.(acts_set). *)
-(* Notation "'psc'" := G.(imm.psc). *)
-(* Notation "'hb'" := G.(imm_hb.hb). *)
-(* Notation "'rf'" := G.(rf). *)
-(* Notation "'lab'" := G.(lab). *)
-(* Notation "'vf'" := (Gvf G). *)
-(* Notation "'W'" := (fun a => is_true (is_w lab a)). *)
-(* Notation "'F'" := (fun a => is_true (is_f lab a)). *)
-(* Notation "'Sc'" := (fun a => is_true (is_sc lab a)). *)
+Notation "'E'" := G.(acts_set).
+Notation "'psc'" := G.(imm.psc).
+Notation "'hb'" := G.(imm_hb.hb).
+Notation "'rf'" := G.(rf).
+Notation "'eco'" := G.(Execution_eco.eco).
+Notation "'lab'" := G.(lab).
+Notation "'vf'" := (Gvf G).
+Notation "'W'" := (fun a => is_true (is_w lab a)).
+Notation "'F'" := (fun a => is_true (is_f lab a)).
+Notation "'Sc'" := (fun a => is_true (is_sc lab a)).
 
-(* Lemma GvfE : vf ≡ ⦗ E ⦘ ⨾ vf ⨾ ⦗ E ⦘. *)
+Notation "'ar'" := (imm.ar G).
+
+Lemma GvfE : vf ≡ ⦗ E ⦘ ⨾ vf ⨾ ⦗ E ⦘.
+Proof.
+  apply AvfE.
+  all: eapply dom_l.
+  { apply WF.(wf_rfE). }
+  { apply WF.(wf_hbE). }
+  apply WF.(wf_pscE).
+Qed.
+
+Lemma Gvf_hb_irr : irreflexive (vf ⨾ hb).
+Proof.
+  unfold Gvf, Avf.
+  arewrite_id ⦗W⦘.
+  arewrite_id ⦗E⦘.
+  arewrite_id ⦗F ∩₁ Sc⦘.
+  rels.
+  
+  arewrite (rf ⊆ eco).
+  generalize (eco_trans WF); ins; relsf.
+
+  generalize (@hb_trans G); ins; relsf.
+
+  assert (irreflexive hb) by (by apply hb_irr).
+  assert (irreflexive (eco ;; hb)).
+  { rotate 1. apply COH. }
+  assert (irreflexive (eco ⨾ psc ⨾ hb)).
+  { rewrite (wf_pscD), !seqA.
+    rewrite (dom_r (wf_ecoD WF)). type_solver. }
+  assert (irreflexive (psc ⨾ hb)).
+  { unfold imm.psc.
+    arewrite_id ⦗F ∩₁ Sc⦘.
+    relsf.
+    rotate 2.
+    generalize (@hb_trans G); ins; relsf. }
+  assert (irreflexive (hb ⨾ psc ⨾ hb)).
+  { rotate 2.
+    generalize (@hb_trans G); ins; relsf. }
+
+  repeat (rewrite crE; relsf; apply irreflexive_union; split; auto).
+  rewrite wf_pscD, !seqA.
+  rotate 2.
+  arewrite (⦗F ∩₁ Sc⦘ ⨾ hb ⨾ eco ⨾ hb ⨾ ⦗F ∩₁ Sc⦘ ≡ psc).
+  arewrite (psc ⊆ ar).
+  arewrite (ar ⊆ ar⁺). rewrite ct_ct.
+  apply ACYC_EXT.
+Qed.
+
+(* Lemma Gvf_hb_sc_hb_irr : irreflexive (vf ⨾ hb ⨾ (psc ⨾ hb)^?). *)
 (* Proof. *)
-(*   split; [|basic_solver]. *)
+(*   case_refl _. *)
+(*   { apply Gvf_hb_irr. } *)
+(*   arewrite (vf ⨾ hb ⨾ psc ⊆ vf). *)
+(*   2: by apply Gvf_hb_irr. *)
 (*   unfold Gvf, Avf. *)
-(*   rewrite (wf_rfE WF) at 1. *)
-(*   rewrite (wf_hbE WF) at 1 2. *)
-(*   rewrite (wf_pscE WF) at 1. *)
-(*   basic_solver 21. *)
+(*   arewrite_id ⦗E⦘ at 1. rels. *)
+(*   rewrite (dom_r WF.(wf_pscE)) at 2. *)
+(*   arewrite ((hb ⨾ ⦗F ∩₁ Sc⦘)^? ⨾ psc^? ⨾ hb^? ⨾ hb ⨾ psc ⊆ *)
+(*                             (hb ⨾ ⦗F ∩₁ Sc⦘)^? ⨾ psc^? ⨾ hb^?). *)
+(*   2: done. *)
+(*   repeat rewrite crE. relsf. *)
 (* Qed. *)
 
-(* Lemma vf_hb_irr l WF WF_SC CSC COH ACYC_EXT: irreflexive (vf ⨾ hb). *)
-(* Proof. *)
-(*   unfold vf. *)
-(*   arewrite_id ⦗W_ l⦘. *)
-(*   arewrite_id ⦗F ∩₁ Sc⦘. *)
-(*   rels. *)
-
-(*   arewrite (rf^? ⊆ eco^?). *)
-(*   generalize (eco_trans WF); ins; relsf. *)
-(*   rewrite (crE sc). *)
-
-(*   generalize (@hb_trans G); ins; relsf. *)
-
-(*   relsf; repeat (splits; try apply irreflexive_union). *)
-(*   - *)
-(*       by rotate 1; apply COH. *)
-(*   -  *)
-(*     rewrite crE at 1; relsf; repeat (splits; try apply irreflexive_union). *)
-(*     * rotate 1; relsf. *)
-(*       rewrite (wf_scD WF_SC). *)
-(*       rotate 1. *)
-(*       sin_rewrite (f_sc_hb_f_sc_in_sc WF WF_SC ACYC_EXT). *)
-(*       destruct WF_SC; relsf. *)
-(*     *  *)
-(*       rewrite (wf_scD WF_SC), !seqA. *)
-(*       rewrite crE; relsf; apply irreflexive_union; splits; *)
-(*         [rewrite (dom_r (wf_ecoD WF)); type_solver|]. *)
-(*       revert CSC; unfold coh_sc; basic_solver 21. *)
-(* Qed. *)
-
-(* Lemma Gvf_hb_sc_hb_irr WF WF_SC CSC COH ACYC_EXT l: *)
-(*   irreflexive (vf ⨾ hb ⨾ (sc ⨾ hb)^?). *)
-(* Proof. *)
-(* case_refl _. *)
-(* apply (vf_hb_irr WF WF_SC CSC COH ACYC_EXT). *)
-(* arewrite (vf ⨾ hb ⨾ sc ⊆ vf). *)
-(* generalize (@vf_hb_sc_hb l WF WF_SC ACYC_EXT); basic_solver 21. *)
-(* apply (vf_hb_irr WF WF_SC CSC COH ACYC_EXT). *)
-(* Qed. *)
-
-(* End GvfProperties. *)
+End GvfProperties.
