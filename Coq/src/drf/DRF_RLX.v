ce2659085fefbb44e9744d1bd9ee875286984ca1
Ilya Kaysin <demarkok@gmail.com>
2019-06-14 16:03:50 +0300

    Fix style.

--- Coq/src/drf/DRF_RLX.v
+++ Coq/src/drf/DRF_RLX.v
@@ -27,39 +27,38 @@ Proof.
 Admitted.
 
 Lemma rf_in_jf (S : ES.t) (X : eventid -> Prop)
-      (wf : ES.Wf S)
-      (exec : Execution.t S X)
-      (jf_in_hb : S.(ES.jf) ⊆ S.(hb)):
+      (WF : ES.Wf S)
+      (EXEC : Execution.t S X)
+      (JF_IN_HB : S.(ES.jf) ⊆ S.(hb)):
   (⦗X⦘ ⨾ S.(ES.rf) ⨾ ⦗X⦘) ⊆ S.(ES.jf).
 Proof.
   unfolder; intros x y H. 
-  destruct H as [x_in_X H]; destruct H as [rf_x_y  y_in_X].
+  destruct H as [x_in_X [rf_x_y y_in_X]].
   unfold ES.rf in rf_x_y; unfolder in rf_x_y.
-  destruct rf_x_y as [H not_cf_x_y];
-  destruct H as [z H];
-  destruct H as [ew_x_z jf_z_y].
-  specialize (jf_in_hb z y jf_z_y) as hb_z_y.
+  destruct rf_x_y as [[z [ew_x_z jf_z_y]] n_cf].
+  specialize (JF_IN_HB z y jf_z_y) as hb_z_y.
   assert (z_in_X: X z).
-  { specialize (Execution.hb_prcl S X exec); intro H; unfolder in H; eauto with hahn. }
-  specialize (ES.ewc wf). intro H. unfolder in H. specialize (H x z ew_x_z). destruct H.
+  { specialize (Execution.hb_prcl S X EXEC).
+    intro H. unfolder in H. eauto with hahn. }
+  eapply ES.ewc in ew_x_z as H. 2: auto. destruct H.
   { basic_solver. }
-  specialize (Execution.ex_ncf S X exec) as cf_free.
+  specialize (Execution.ex_ncf S X EXEC) as cf_free.
   destruct cf_free with (x := x) (y := z); basic_solver.
 Qed.
 
 Lemma po_rf_acyclic (S : ES.t) (X : eventid -> Prop)
-      (wf : ES.Wf S)
-      (cons : es_consistent S (m:=Weakestmo))
-      (exec : Execution.t S X)
-      (jf_in_hb : S.(ES.jf) ⊆ S.(hb)):
+      (WF : ES.Wf S)
+      (CONS : es_consistent S (m := Weakestmo))
+      (EXEC : Execution.t S X)
+      (JF_IN_HB : S.(ES.jf) ⊆ S.(hb)):
   acyclic (⦗X⦘ ⨾ S.(ES.sb) ⨾ ⦗X⦘ ∪ ⦗X⦘ ⨾ S.(ES.rf) ⨾ ⦗X⦘).
 Proof.
-  specialize (rf_in_jf S X wf exec jf_in_hb) as rf_in_jf.
+  specialize (rf_in_jf S X WF EXEC JF_IN_HB) as rf_in_jf.
   assert (hb_acyclic: acyclic S.(hb)).
   { apply trans_irr_acyclic.
     { eapply hb_irr; eauto. }
     eauto with hahn. }
-  erewrite rf_in_jf, jf_in_hb.
+  erewrite rf_in_jf, JF_IN_HB.
   arewrite (S.(ES.sb) ⊆ S.(hb)).
   rewrite <- restr_relE, inclusion_restr.
   by rewrite unionK.
