159e0fada326f00b29c4b08370491391789d8577
Ilya Kaysin <demarkok@gmail.com>
2019-06-17 16:55:14 +0300

    Start the proof of the Lemma 5.

--- Coq/src/drf/DRF_RLX.v
+++ Coq/src/drf/DRF_RLX.v
@@ -8,6 +8,7 @@ Require Import Execution.
 Require Import Step.
 Require Import Race.
 Require Import ProgES.
+Require Import StepWf.
 
 Module DRF.
 
@@ -18,12 +19,61 @@ Definition program_execution P S X :=
 Definition RLX_race_free_program P :=
   (forall S X, program_execution P S X -> Race.rc11_consistent_x S X -> Race.RLX_race_free S X).
 
+Lemma wf_es P
+      (S : ES.t)
+      (STEPS : (step Weakestmo)＊ (prog_es_init P) S):
+  ES.Wf S.
+Proof.
+   specialize (clos_refl_trans_ind_left ES.t
+                                       (step Weakestmo)
+                                       (prog_es_init P)
+                                       (ES.Wf)).
+   intro H. apply H. 
+   { admit.  (* apply prog_init_wf. *) }
+   { admit. }
+   auto.
+Admitted.
+  
 Lemma jf_in_hb P
       (RACE_FREE : RLX_race_free_program P)
       (S : ES.t)
-      (steps : (step Weakestmo)＊ (prog_es_init P) S):
+      (STEPS : (step Weakestmo)＊ (prog_es_init P) S):
   S.(ES.jf) ⊆ S.(hb).
 Proof.
+  Locate "＊". SearchAbout clos_refl_trans.
+  specialize (clos_refl_trans_ind_left ES.t
+                                       (step Weakestmo)
+                                       (prog_es_init P)
+                                       (fun s => s.(ES.jf) ⊆ s.(hb))).
+  intro H. apply H. 1, 3: auto; basic_solver.
+  clear H STEPS S. intros G G' STEPS IH STEP. 
+  assert (WF : ES.Wf G').
+  { eapply wf_es with (P := P). eapply transitive_rt. 2: apply rt_step. all: eauto. } 
+  
+  inversion_clear STEP as [e H]. destruct H as [e']. desc. 
+  assert (q: G.(hb) ⊆ G'.(hb)). { admit. }
+  inversion_clear TT as [H | [H | [H | H]]].
+  1, 3: inversion_clear H; desc; rewrite JF'; basic_solver. 
+  { inversion_clear H as [w]; desc.
+    assert (hh : (G'.(hb)⁼ w e) \/ (not (G'.(hb)⁼ w e))) by admit. 
+    destruct hh as [[eq_w_e | [ hb_w_e | hb_e_w]] | not_hb_w_e].
+    { admit. }
+    { inversion_clear AJF; desc. rewrite JF'. apply inclusion_union_l.
+      { basic_solver. }
+      unfold AddJF.jf_delta. basic_solver. }         
+    { exfalso.
+      inversion_clear BSTEP.
+      assert (coh : (irreflexive (G'.(hb) ⨾ (G'.(eco) Weakestmo)^?))).
+      { apply coh. auto. }
+      destruct coh with (x := e).
+      unfold "⨾". exists w. split.
+      { basic_solver. }
+      { apply r_step. unfold eco. apply ct_step.
+        repeat apply inclusion_union_r1.
+        unfold ES.rf. unfold "\". split.
+        { unfold "⨾". exists w. split.
+          { apply ES.ew_refl; eauto. inversion AJF. desc. admit. }
+          admit.
 
 Admitted.
 
