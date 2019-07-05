4af698e9e4f11059f568560473f4bb14654d8f1b
Ilya Kaysin <demarkok@gmail.com>
2019-07-05 19:01:56 +0300

    Generalize the prog_g_es_init lemmas to the prog_l_es_init lemmas for arbitrary location list.

--- Coq/src/ProgES.v
+++ Coq/src/ProgES.v
@@ -53,10 +53,23 @@ Definition prog_init_K
                                   (rt_refl _ _ (init linstr))) in
        (CInit tid, existT _ (thread_lts tid) st'))
     (RegMap.elements prog).
- 
+
+Definition prog_l_es_init (prog : stable_prog_type) (locs : list location) :=
+  ES.init (undup locs) (prog_init_K prog).
+
 Definition prog_es_init (prog : stable_prog_type) :=
-  ES.init (prog_locs (stable_prog_to_prog prog))
-          (prog_init_K prog).
+  prog_l_es_init prog (prog_locs (stable_prog_to_prog prog)).
+
+
+Lemma prog_es_init_alt (prog : stable_prog_type) :
+  prog_es_init prog = ES.init
+                        (prog_locs (stable_prog_to_prog prog))
+                        (prog_init_K prog).
+Proof.
+  unfold prog_es_init, prog_l_es_init, prog_locs.
+  rewrite undup_nodup; eauto.
+  apply NoDup_nodup.
+Qed.
 
 Definition g_locs (G : execution) :=
   undup (flatten (map (fun e =>
@@ -67,53 +80,84 @@ Definition g_locs (G : execution) :=
                       (acts G))).
 
 Definition prog_g_es_init prog (G : execution) :=
-  ES.init (g_locs G) (prog_init_K prog).
+  prog_l_es_init prog (g_locs G).
+
+Lemma prog_g_es_init_alt prog (G : execution) :
+  prog_g_es_init prog G = ES.init (g_locs G) (prog_init_K prog).
+Proof.
+  unfold prog_g_es_init, prog_l_es_init, g_locs.
+  rewrite undup_nodup; auto. 
+Qed.
+
+Lemma prog_l_es_init_ninit locs prog :
+  ES.acts_ninit_set (prog_l_es_init prog locs) ≡₁ ∅.
+Proof.
+  split; [|basic_solver].
+  red. unfold prog_l_es_init, ES.init. intros x HH. 
+  apply HH. red. split; auto.
+  apply HH.
+Qed. 
 
 Lemma prog_g_es_init_ninit G prog :
   ES.acts_ninit_set (prog_g_es_init prog G) ≡₁ ∅.
+Proof. apply prog_l_es_init_ninit. Qed.
+
+Lemma prog_l_es_init_sb locs prog :
+  ES.sb (prog_l_es_init prog locs) ≡ ∅₂.
 Proof.
   split; [|basic_solver].
-  red. unfold prog_g_es_init, ES.init. intros x HH. 
-  apply HH. red. split; auto.
-  apply HH.
+  unfold prog_l_es_init, ES.init. simpls.
 Qed.
 
 Lemma prog_g_es_init_sb G prog :
   ES.sb (prog_g_es_init prog G) ≡ ∅₂.
+Proof. apply prog_l_es_init_sb. Qed.
+  
+Lemma prog_l_es_init_jf locs prog :
+  ES.jf (prog_l_es_init prog locs) ≡ ∅₂.
 Proof.
   split; [|basic_solver].
-  unfold prog_g_es_init, ES.init. simpls.
+  unfold prog_l_es_init, ES.init. simpls.
 Qed.
 
 Lemma prog_g_es_init_jf G prog :
   ES.jf (prog_g_es_init prog G) ≡ ∅₂.
+Proof. apply prog_l_es_init_jf. Qed.
+
+Lemma prog_l_es_init_sw locs prog :
+  sw (prog_l_es_init prog locs) ≡ ∅₂.
 Proof.
   split; [|basic_solver].
-  unfold prog_g_es_init, ES.init. simpls.
+  unfold sw. rewrite prog_l_es_init_jf. basic_solver.
 Qed.
 
 Lemma prog_g_es_init_sw G prog :
   sw (prog_g_es_init prog G) ≡ ∅₂.
+Proof. apply prog_l_es_init_sw. Qed.
+
+Lemma prog_l_es_init_hb locs prog :
+  hb (prog_l_es_init prog locs) ≡ ∅₂.
 Proof.
   split; [|basic_solver].
-  unfold sw. rewrite prog_g_es_init_jf. basic_solver.
+  unfold hb.
+  rewrite prog_l_es_init_sw, prog_l_es_init_sb.
+  rewrite ct_no_step; basic_solver.
 Qed.
 
 Lemma prog_g_es_init_hb G prog :
   hb (prog_g_es_init prog G) ≡ ∅₂.
+Proof. apply prog_l_es_init_hb. Qed.
+
+Lemma prog_l_es_init_cf locs prog :
+  ES.cf (prog_l_es_init prog locs) ≡ ∅₂.
 Proof.
   split; [|basic_solver].
-  unfold hb.
-  rewrite prog_g_es_init_sw, prog_g_es_init_sb.
-  rewrite ct_no_step; basic_solver.
+  unfold ES.cf. rewrite prog_l_es_init_ninit. basic_solver.
 Qed.
 
 Lemma prog_g_es_init_cf G prog :
   ES.cf (prog_g_es_init prog G) ≡ ∅₂.
-Proof.
-  split; [|basic_solver].
-  unfold ES.cf. rewrite prog_g_es_init_ninit. basic_solver.
-Qed.
+Proof. apply prog_l_es_init_cf. Qed.
 
 Hint Rewrite prog_g_es_init_ninit
      prog_g_es_init_sb
@@ -123,30 +167,43 @@ Hint Rewrite prog_g_es_init_ninit
      prog_g_es_init_cf
   : prog_g_es_init_db.
 
-Lemma prog_g_es_init_consistent G prog :
-  @es_consistent (prog_g_es_init prog G) Weakestmo.
+Hint Rewrite prog_l_es_init_ninit
+     prog_l_es_init_sb
+     prog_l_es_init_jf
+     prog_l_es_init_sw
+     prog_l_es_init_hb
+     prog_l_es_init_cf
+  : prog_l_es_init_db.
+
+Lemma prog_l_es_init_consistent locs prog :
+  @es_consistent (prog_l_es_init prog locs) Weakestmo.
 Proof.
   constructor; unfold ecf, ES.jfe, ES.icf.
-  all: autorewrite with prog_g_es_init_db; auto.
+  all: autorewrite with prog_l_es_init_db; auto.
   all: basic_solver.
 Qed.
 
-Lemma prog_g_es_init_act_in prog G
-      e (ACT : ES.acts_set (prog_g_es_init prog G) e) :
+
+Lemma prog_g_es_init_consistent G prog :
+  @es_consistent (prog_g_es_init prog G) Weakestmo.
+Proof. apply prog_l_es_init_consistent. Qed.
+
+Lemma prog_l_es_init_act_in prog locs 
+      e (ACT : ES.acts_set (prog_l_es_init prog locs) e) :
   exists l,
     In (e, init_write l)
        (indexed_list
-          (map init_write (g_locs G))).
+          (map init_write (undup locs))).
 Proof.
   ins.
   assert
     (exists b,
         In (e, b) (indexed_list
-                     (map init_write (g_locs G))))
+                     (map init_write (undup locs))))
     as [b IN].
   { apply indexed_list_range. desf. }
 
-  assert (In b (map init_write (g_locs G)))
+  assert (In b (map init_write (undup locs)))
     as BIN.
   { clear -IN.
     apply In_map_snd in IN.
@@ -157,60 +214,90 @@ Proof.
   eauto.
 Qed.
 
-Lemma prog_g_es_init_act_lab prog G
+Lemma prog_g_es_init_act_in prog G
       e (ACT : ES.acts_set (prog_g_es_init prog G) e) :
-  exists l, ES.lab (prog_g_es_init prog G) e = Astore Xpln Opln l 0.
+  exists l,
+    In (e, init_write l)
+       (indexed_list
+          (map init_write (g_locs G))).
 Proof.
-  apply prog_g_es_init_act_in in ACT. destruct ACT as [l LL].
+  apply prog_l_es_init_act_in in ACT.
+  unfold g_locs in *.
+  rewrite undup_nodup in ACT; auto.
+Qed.
+
+Lemma prog_l_es_init_act_lab prog locs 
+      e (ACT : ES.acts_set (prog_l_es_init prog locs) e) :
+  exists l, ES.lab (prog_l_es_init prog locs) e = Astore Xpln Opln l 0.
+Proof.
+  apply prog_l_es_init_act_in in ACT. destruct ACT as [l LL].
   exists l. unfold ES.lab, prog_g_es_init, ES.init.
   apply l2f_in; desf.
   apply indexed_list_fst_nodup.
+Qed.
+
+Lemma prog_g_es_init_act_lab prog G
+      e (ACT : ES.acts_set (prog_g_es_init prog G) e) :
+  exists l, ES.lab (prog_g_es_init prog G) e = Astore Xpln Opln l 0.
+Proof. by apply prog_l_es_init_act_lab. Qed.  
+
+Lemma prog_l_es_init_w locs prog :
+  ES.acts_set (prog_l_es_init prog locs) ≡₁
+  ES.acts_set (prog_l_es_init prog locs) ∩₁
+  (fun a => is_true (is_w (ES.lab (prog_l_es_init prog locs)) a)).
+Proof.
+  split; [|basic_solver].
+  unfolder. intros. split; auto. 
+  unfold is_w.
+  apply prog_l_es_init_act_lab in H. desf.
 Qed.
 
 Lemma prog_g_es_init_w G prog :
   ES.acts_set (prog_g_es_init prog G) ≡₁
   ES.acts_set (prog_g_es_init prog G) ∩₁
   (fun a => is_true (is_w (ES.lab (prog_g_es_init prog G)) a)).
-Proof.
-  split; [|basic_solver].
-  unfolder. intros. split; auto. 
-  unfold is_w.
-  apply prog_g_es_init_act_lab in H. desf.
-Qed.
+Proof. apply prog_l_es_init_w. Qed.
 
-Lemma prog_g_es_seqn G prog x : ES.seqn (prog_g_es_init prog G) x = 0.
+Lemma prog_l_es_seqn locs prog x : ES.seqn (prog_l_es_init prog locs) x = 0.
 Proof.
-  unfold ES.seqn. autorewrite with prog_g_es_init_db; eauto.
+  unfold ES.seqn. autorewrite with prog_l_es_init_db; eauto.
   relsf.
   apply countNatP_empty.
 Qed.
 
+Lemma prog_g_es_seqn G prog x : ES.seqn (prog_g_es_init prog G) x = 0.
+Proof. apply prog_l_es_seqn. Qed.
+
+Lemma prog_l_es_init_init locs prog :
+  ES.acts_set (prog_l_es_init prog locs) ≡₁
+  ES.acts_init_set (prog_l_es_init prog locs).
+Proof. unfold ES.acts_init_set. simpls. basic_solver. Qed.
+
 Lemma prog_g_es_init_init G prog :
   ES.acts_set (prog_g_es_init prog G) ≡₁
   ES.acts_init_set (prog_g_es_init prog G).
-Proof. unfold ES.acts_init_set. simpls. basic_solver. Qed.
-  
-Lemma prog_g_es_init_wf G prog (nInitProg : ~ IdentMap.In tid_init prog) :
-  ES.Wf (prog_g_es_init prog G).
-Proof.
+Proof. apply prog_l_es_init_init. Qed.
+
+Lemma prog_l_es_init_wf locs prog (nInitProg : ~ IdentMap.In tid_init prog):
+  ES.Wf (prog_l_es_init prog locs).
   assert
-    (NoDup (map init_write (g_locs G)))
+    (NoDup (map init_write (undup locs)))
     as NNDD.
   { apply nodup_map.
     2: { ins. intros HH. inv HH. }
     unfold g_locs. apply nodup_undup. }
   constructor.
-  all: autorewrite with prog_g_es_init_db; auto.
+  all: autorewrite with prog_l_es_init_db; auto.
   all: simpls.
   all: try basic_solver.
   { ins. red. exists b.
     splits; auto.
     red. split; auto. }
   { intros e [AA BB]. 
-    eapply prog_g_es_init_act_lab; eauto. }
+    eapply prog_l_es_init_act_lab; eauto. }
   { red. ins.
-    destruct SX as [SX _]. apply prog_g_es_init_act_in in SX.
-    destruct SY as [SY _]. apply prog_g_es_init_act_in in SY.
+    destruct SX as [SX _]. apply prog_l_es_init_act_in in SX.
+    destruct SY as [SY _]. apply prog_l_es_init_act_in in SY.
     desf.
     assert (l0 = l); subst.
     { unfold loc, init_write in *.
@@ -224,11 +311,11 @@ Proof.
   { unfolder. ins. eexists.
     splits; eauto.
     2: by red.
-    apply prog_g_es_seqn. }
-  { rewrite prog_g_es_init_w. type_solver. }
+    apply prog_l_es_seqn. }
+  { rewrite prog_l_es_init_w. type_solver. }
   { intros ol a b [[EA _] WA] [[EB _] WB].
-    set (CA := EA). apply prog_g_es_init_act_in in CA. desf.
-    set (CB := EB). apply prog_g_es_init_act_in in CB. desf.
+    set (CA := EA). apply prog_l_es_init_act_in in CA. desf.
+    set (CB := EB). apply prog_l_es_init_act_in in CB. desf.
     assert (l0 = l); subst.
     { unfold loc, init_write in *.
       erewrite l2f_in in WB; eauto.
@@ -241,10 +328,10 @@ Proof.
     eapply indexed_list_snd_nodup; eauto. }
   { split; [|basic_solver].
     unfolder. ins. desf. splits; auto.
-    all: eapply prog_g_es_init_w; eauto.
+    all: eapply prog_l_es_init_w; eauto.
     Unshelve. all: auto. }
   { intros HH. desf.
-    unfold prog_g_es_init, ES.init, ES.cont_thread, ES.cont_set in *. 
+    unfold prog_l_es_init, ES.init, ES.cont_thread, ES.cont_set in *. 
     simpls.
     unfold prog_init_K in KK.
     apply in_map_iff in KK.
@@ -254,7 +341,7 @@ Proof.
     apply RegMap.Facts.in_find_iff.
     rewrite KK0. desf. }
   { intros HH. desf. inv RMW. }
-  { unfold prog_g_es_init, ES.init, ES.cont_thread, ES.cont_set in *. 
+  { unfold prog_l_es_init, ES.init, ES.cont_thread, ES.cont_set in *. 
     simpls.
     unfold prog_init_K in *.
     ins.
@@ -264,13 +351,21 @@ Proof.
     apply RegMap.elements_complete in CK0.
     apply RegMap.elements_complete in CK'0.
     simpls; desf. }
-  { ins. by apply prog_g_es_init_ninit in EE. }
+  { ins. by apply prog_l_es_init_ninit in EE. }
   ins. exfalso.
   red in inK.
-  unfold prog_g_es_init, ES.init in *. simpls.
+  unfold prog_l_es_init, ES.init in *. simpls.
   unfold prog_init_K in *.
   apply in_map_iff in inK. desf.
-Qed.
+Qed. 
+
+Lemma prog_g_es_init_wf G prog (nInitProg : ~ IdentMap.In tid_init prog) :
+  ES.Wf (prog_g_es_init prog G).
+Proof. by apply prog_l_es_init_wf. Qed.
+
+Lemma prog_es_init_wf prog (nInitProg : ~ IdentMap.In tid_init prog) :
+  ES.Wf (prog_es_init prog).
+Proof. by apply prog_l_es_init_wf. Qed.
 
 Lemma prog_g_es_init_same_lab prog G (WF : Wf G) :
   eq_dom (ES.acts_set (prog_g_es_init prog G))
@@ -278,21 +373,23 @@ Lemma prog_g_es_init_same_lab prog G (WF : Wf G) :
          (Execution.lab G ∘ e2a (prog_g_es_init prog G)).
 Proof.
   red. ins.
+  arewrite (undup (g_locs G) = g_locs G).
+  { unfold g_locs. rewrite undup_nodup; auto. }
   unfold compose.
   apply prog_g_es_init_act_in in SX. desf.
-
-  unfold prog_g_es_init, e2a, ES.init, ES.acts_set in *; simpls; desf.
+  rewrite prog_g_es_init_alt.
+  unfold e2a, ES.init, ES.acts_set in *; simpls; desf.
   unfold Events.loc.
   erewrite l2f_in; [|by apply indexed_list_fst_nodup|by eauto].
   simpls. rewrite wf_init_lab; auto.
 Qed.
 
-Lemma prog_g_es_init_K prog G k state
+Lemma prog_l_es_init_K prog locs k state
       (INK : ES.cont_set
-               (prog_g_es_init prog G)
+               (prog_l_es_init prog locs)
                (k, existT
                      Language.state
-                     (thread_lts (ES.cont_thread (prog_g_es_init prog G)
+                     (thread_lts (ES.cont_thread (prog_l_es_init prog locs)
                                                  k))
                      state)) :
   exists thread,
@@ -304,7 +401,7 @@ Proof.
                  (OO : (c, a) = (c, b)), a = b) as OO.
   { ins. inv OO. }
   ins. red in INK.
-  unfold prog_g_es_init, ES.init, prog_init_K, ES.cont_thread in *.
+  unfold prog_l_es_init, ES.init, prog_init_K, ES.cont_thread in *.
   simpls.
   apply in_map_iff in INK. desc. inv INK.
   destruct x. simpls. desf.
@@ -327,15 +424,29 @@ Proof.
   apply eps_steps_in_steps. eauto.
 Qed.
 
-Lemma prog_g_es_init_lab prog G e :
-  << ELAB : ES.lab (prog_g_es_init prog G) e = Afence Orlx >> \/
+Lemma prog_g_es_init_K prog G k state
+      (INK : ES.cont_set
+               (prog_g_es_init prog G)
+               (k, existT
+                     Language.state
+                     (thread_lts (ES.cont_thread (prog_g_es_init prog G)
+                                                 k))
+                     state)) :
+  exists thread,
+    ⟪ KTID  : k = CInit thread ⟫ /\
+    ⟪ STEPS : (istep thread [])＊ (init (instrs state)) state ⟫ /\
+    ⟪ STBL  : stable_state state ⟫.
+Proof. by apply prog_l_es_init_K. Qed.
+
+Lemma prog_l_es_init_lab prog locs e :
+  << ELAB : ES.lab (prog_l_es_init prog locs) e = Afence Orlx >> \/
   exists l,
-  << ELAB : ES.lab (prog_g_es_init prog G) e = init_write l >>.
+  << ELAB : ES.lab (prog_l_es_init prog locs) e = init_write l >>.
 Proof.
-  unfold prog_g_es_init, ES.init. simpls.
+  unfold prog_l_es_init, ES.init. simpls.
   unnw.
   edestruct @l2f_v with (A:=nat)
-                        (l:=indexed_list (map init_write (g_locs G)))
+                        (l:=indexed_list (map init_write (undup locs)))
                         (a:=e)
                         (DEC:=Nat.eq_dec).
   { apply indexed_list_fst_nodup. }
@@ -346,6 +457,12 @@ Proof.
   unfold indexed_list in *.
   remember 0 as n. clear Heqn.
   generalize dependent n.
-  induction (g_locs G); simpls.
+  induction (undup locs); simpls.
   ins. desf; eauto.
 Qed.
+
+Lemma prog_g_es_init_lab prog G e :
+  << ELAB : ES.lab (prog_g_es_init prog G) e = Afence Orlx >> \/
+  exists l,
+  << ELAB : ES.lab (prog_g_es_init prog G) e = init_write l >>.
+Proof. apply prog_l_es_init_lab. Qed.
