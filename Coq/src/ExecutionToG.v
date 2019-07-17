c9d22b7b24b935b87105ffe1a330926cf0f951c8
Ilya Kaysin <demarkok@gmail.com>
2019-07-17 20:41:37 +0300

    Add a2e_e2a Lemma.

--- Coq/src/ExecutionToG.v
+++ Coq/src/ExecutionToG.v
@@ -14,7 +14,19 @@ Require Import Omega.
 Local Open Scope program_scope.
 
 Section ExecutionToG.
-    
+
+Lemma nodup_first_nat_list : forall n : nat, NoDup (first_nat_list n).
+Proof.
+  induction n.
+  { apply NoDup_nil. }
+  apply NoDup_cons; auto.
+  rewrite first_nat_list_In_alt.
+  omega. 
+Qed.
+  
+Definition eventid_list S (X : eventid -> Prop) :=
+  filterP X (first_nat_list (ES.next_act S)).
+  
 Variable S : ES.t.
 Variable X : eventid -> Prop.
 Variable G : execution.
@@ -135,7 +147,7 @@ Proof.
 Qed.
 
 Definition a2e : actid -> eventid :=
-  let e_list := nat_filter X (ES.next_act S) in
+  let e_list := eventid_list S X in
   let a_list := map (e2a S) e_list in
   list_to_fun (fun x y => excluded_middle_informative (x = y)) 
               (ES.next_act S)
@@ -167,7 +179,25 @@ Proof.
   unfold fst. basic_solver.
 Qed.
 
-Lemma a2e_e2a : eq_dom X (a2e ∘ (e2a S)) id.
+Lemma Injective_map_NoDup_dom {A B} (P : A -> Prop) (f : A -> B) (l : list A)
+      (IJ : inj_dom P f)
+      (PL : Forall P l)
+      (NO_DUP: NoDup l) :
+  NoDup (map f l).
+Proof.
+  generalize dependent NO_DUP.
+  induction 1 as [|x l SX N IH]; simpls;
+    constructor; apply Forall_cons in PL; desf; auto.
+  rewrite in_map_iff. intros (y & E & Y). apply IJ in E.
+  { now subst. }
+  { eapply Forall_forall; eauto. }
+  auto.
+Qed.
+
+Lemma a2e_e2a
+      (WF : ES.Wf S)
+      (EXEC : Execution.t S X) :
+  eq_dom X (a2e ∘ (e2a S)) id.
 Proof.
   red. ins.
   unfold id, "∘".
@@ -177,12 +207,30 @@ Proof.
     { ins. specialize (split_as_map l). ins. rewrite H. auto. }
     rewrite combine_split_l.
     2: { rewrite map_length. auto. }
-    apply Injective_map_NoDup.
-    2: { unfold nat_filter. }
-    
-    
-
-    unfold upd_with_lists, upd_with_list.
+    apply Injective_map_NoDup_dom with (P := X).
+    { destruct EXEC. apply e2a_inj; auto. }  
+    { apply ForallE. intros y HH. apply in_filterP_iff in HH. desf. }
+    apply nodup_filterP, nodup_first_nat_list. }
+  assert (X_IN : In x (eventid_list S X)).
+  { apply in_filterP_iff. split; auto.
+    destruct EXEC. rewrite ES.E_alt in ex_inE.
+    basic_solver. }
+  eapply In_nth in X_IN.
+  desf.
+  assert (HH : (e2a S x, x) =
+               (nth n
+                    (combine
+                       (map (e2a S) (eventid_list S X))
+                       (eventid_list S X))
+                    (e2a S x, x))).
+  { rewrite combine_nth; [|by rewrite map_length].
+    by rewrite map_nth, X_IN0. }
+  rewrite HH. eapply nth_In. rewrite length_combine.
+  rewrite map_length. 
+  apply Nat.min_glb_iff.
+  omega. 
+Qed. 
+  
   
 Lemma e2a_lab_pred p (x2g : X2G) :
    e2a S □₁ (X ∩₁ (p ∘ Slab)) ⊆₁ p ∘ Glab .
@@ -197,9 +245,6 @@ Qed.
     
     
 End ExecutionToG.
-
-
-
 
 
 Definition X2G_fun (S : ES.t) (X : eventid -> Prop) : execution :=
