b61b4d0cf82e30cbcb30ef3de177c4db5a9b118a
Ilya Kaysin <demarkok@gmail.com>
2019-08-30 19:17:10 +0300

    WIP: X2G_Race lemma.

---
+++ Coq/src/drf/Race_G.v
@@ -0,0 +1,35 @@
+From hahn Require Import Hahn.
+From imm Require Import Events Execution imm_s_hb Prog ProgToExecutionProperties RC11.
+
+Require Import SC.
+
+Require Import AuxRel. (* ⁼ *)
+
+Set Implicit Arguments.
+
+Section Race_G.
+
+Variable G : execution.
+
+Notation "'E'" := G.(acts_set).
+Notation "'lab'" := G.(lab).
+Notation "'hb'" := G.(hb).
+
+Notation "'W'" := (fun a => is_true (is_w lab a)).
+
+Notation "'Sc'" := (fun a => is_true (is_sc lab a)).
+
+Notation "'same_loc'" := (same_loc lab).
+
+Definition one {A : Type} (X : A -> Prop) a b := X a \/ X b.
+
+Definition race :=
+  dom_rel (((E × E) \ hb⁼) ∩ same_loc ∩ one W).
+
+Definition RA_race_free_G :=
+  race ⊆₁ Sc.
+
+End Race_G.
+
+Definition SC_RA_race_free_program_G P :=
+  (forall G, ProgToExecutionProperties.program_execution P G -> sc_consistent G -> RA_race_free_G G).
