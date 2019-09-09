fcdc2252e01b03f401e8179f7f707cc3f6947ab5
Ilya Kaysin <demarkok@gmail.com>
2019-09-09 12:04:35 +0300

    Renaming.

--- Coq/src/drf/Race_G.v
+++ Coq/src/drf/Race_G.v
@@ -16,8 +16,12 @@ Notation "'lab'" := G.(lab).
 Notation "'hb'" := G.(hb).
 
 Notation "'W'" := (fun a => is_true (is_w lab a)).
+Notation "'R'" := (fun a => is_true (is_r lab a)).
 
 Notation "'Sc'" := (fun a => is_true (is_sc lab a)).
+Notation "'Rel'" := (is_rel lab).
+Notation "'Acq'" := (is_acq lab).
+Notation "'Acqrel'" := (is_acqrel lab).
 
 Notation "'same_loc'" := (same_loc lab).
 
@@ -26,10 +30,16 @@ Definition one {A : Type} (X : A -> Prop) a b := X a \/ X b.
 Definition race :=
   dom_rel (((E × E) \ hb⁼) ∩ same_loc ∩ one W).
 
-Definition RA_race_free_G :=
+Definition ra_race_free_G :=
   race ⊆₁ Sc.
+
+Definition rlx_race_free_G :=
+  race ⊆₁ (Rel ∩₁ W) ∪₁ (Acq ∩₁ R).
 
 End Race_G.
 
-Definition SC_RA_race_free_program_G P :=
-  (forall G, ProgToExecutionProperties.program_execution P G -> sc_consistent G -> RA_race_free_G G).
+Definition sc_ra_race_free_program_G P :=
+  (forall G, ProgToExecutionProperties.program_execution P G -> sc_consistent G -> ra_race_free_G G).
+
+Definition rc11_rlx_race_free_program_G P :=
+  (forall G, ProgToExecutionProperties.program_execution P G -> rc11_consistent G -> rlx_race_free_G G).
